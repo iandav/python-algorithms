99e346b55b35ac36016987d3409bba79eaff3d20
Ian <iandavid.dev@protonmail.com>
2022-06-16 11:18:17 -0300

    Hash table with linear probing completed

--- hash_table/hash_table.py
+++ hash_table/hash_table.py
@@ -95,3 +95,62 @@ class BasicHashTable:
 #
 # 3. While UPDATING a key-value pair, we apply the same strategy, but instead of searching for an empty location,
 #    we look for a location which contains a key-value pair with the matching key, and update its value.
+
+# Hash Table with Linear Probing
+
+def get_valid_index(data_list, key):
+    # Start with the index returned by get_index
+    idx = get_index(data_list, key)
+    
+    while True:
+        # Get the key-value pair stored at idx
+        kv = data_list[idx]
+        
+        # If it is None, return the index
+        if kv is None:
+            return idx
+        
+        # If the stored key matches the given key, return the index
+        k, v = kv
+        if kv[0] == key:
+            return idx
+        
+        # Move to the next index
+        idx += 1
+        
+        # Go back to the start if you have reached the end of the array
+        if idx == len(data_list):
+            idx = 0
+
+class ProbingHashTable:
+    def __init__(self, max_size=MAX_HASH_TABLE_SIZE):
+        # 1. Create a list of size `max_size` with all values None
+        self.data_list = [None] * max_size
+     
+    def insert(self, key, value):
+        # 1. Find the index for the key using get_valid_index
+        idx = get_valid_index(self.data_list, key)
+        
+        # 2. Store the key-value pair at the right index
+        self.data_list[idx] = key, value
+    
+    def find(self, key):
+        # 1. Find the index for the key using get_valid_index
+        idx = get_valid_index(self.data_list, key)
+        
+        # 2. Retrieve the data stored at the index
+        kv = self.data_list[idx]
+        
+        # 3. Return the value if found, else return None
+        return None if kv is None else kv[1]
+    
+    def update(self, key, value):
+        # 1. Find the index for the key using get_valid_index
+        idx = get_valid_index(self.data_list, key)
+        
+        # 2. Store the new key-value pair at the right index
+        self.data_list[idx] = key, value
+
+    def list_all(self):
+        # 1. Extract the key from each key-value pair 
+        return [kv[0] for kv in self.data_list if kv is not None]
